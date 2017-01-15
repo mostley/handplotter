faf988007e94c48f2991138013debe269fe737de
Sven Hecht <sven.hecht@citrix.com>
2017-01-15 10:26:14 +0100

    updated vector class for firmware, added unit testing “framework”

---
+++ software/ArduinoFirmware/test/vector-test.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+#include "vector.cpp"
+
+int test_retention() {
+  printf("=== Testing Vector retention ===\n");
+
+  Vector vector = Vector(15, 25);
+  if (vector.x != 15) {
+    printf("Retention failed. X was not saved in constructor\n");
+    return 1;
+  }
+
+  if (vector.y != 25) {
+    printf("Retention failed. Y was not saved in constructor\n");
+    return 1;
+  }
+
+  return 0;
+}
+
+int test_addition() {
+  printf("=== Testing Vector addition ===\n");
+
+  Vector a = Vector(5, 10);
+  Vector b = Vector(10, 20);
+  Vector c = a + b;
+
+  if (c.x != 15 || c.y != 30) {
+    printf("Addition failed.\nExpected the resulting Vector to be [15, 30] ");
+    printf("instead got [%f, %f]\n", c.x, c.y);
+    return 1;
+  }
+
+  return 0;
+}
+
+int test_substraction() {
+  printf("=== Testing Vector substraction ===\n");
+
+  Vector a = Vector(5, 10);
+  Vector b = Vector(10, 2);
+  Vector c = a - b;
+
+  if (c.x != -5 || c.y != 8) {
+    printf("Substraction failed.\nExpected the resulting Vector to be [-5, 5] ");
+    printf("instead got [%f, %f]\n", c.x, c.y);
+    return 1;
+  }
+
+  return 0;
+}
+
+int test_multiplication() {
+  printf("=== Testing Vector multiplication ===\n");
+
+  Vector a = Vector(4, 10);
+  double b = 1.5;
+  Vector c = a * b;
+
+  if (c.x != 6 || c.y != 15) {
+    printf("Multiplication with double failed.\nExpected the resulting Vector to be [6, 15] ");
+    printf("instead got [%f, %f]\n", c.x, c.y);
+    return 1;
+  }
+
+  int d = 2;
+  c = a * d;
+
+  if (c.x != 8 || c.y != 20) {
+    printf("Multiplication with double failed.\nExpected the resulting Vector to be [8, 20] ");
+    printf("instead got [%f, %f]\n", c.x, c.y);
+    return 1;
+  }
+
+  return 0;
+}
+
+int main() {
+  int error;
+
+  error = test_retention();
+  if (error > 0) { return error; }
+
+  error = test_addition();
+  if (error > 0) { return error; }
+
+  error = test_substraction();
+  if (error > 0) { return error; }
+
+  error = test_multiplication();
+  if (error > 0) { return error; }
+
+  return 0;
+}
--- software/ArduinoFirmware/vector.cpp
+++ software/ArduinoFirmware/vector.cpp
@@ -4,13 +4,42 @@
 struct Vector {
 
 public:
-  int x;
-  int y;
+  double x;
+  double y;
+
+  Vector() {
+    this->x = 0;
+    this->y = 0;
+  }
 
   Vector(double x, double y) {
-    this.x = x;
-    this.y = y;
+    this->x = x;
+    this->y = y;
   }
-}
+
+  Vector add(const Vector& other) {
+    return Vector(this->x + other.x, this->y + other.y);
+  }
+
+  Vector substract(const Vector& other) {
+    return Vector(this->x - other.x, this->y - other.y);
+  }
+
+  Vector multiply(double other) {
+    return Vector(this->x * other, this->y * other);
+  }
+
+  Vector operator+(const Vector& b) {
+    return this->add(b);
+  }
+
+  Vector operator-(const Vector& b) {
+    return this->substract(b);
+  }
+
+  Vector operator*(double b) {
+    return this->multiply(b);
+  }
+};
 
 #endif  /* VECTOR_CPP */
